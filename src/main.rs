80c0b30117240f55445b0456a01ce0b19fc89628
Artem Korolev <artkor@live.ru>
2022-11-08 11:47:32 +0200

    feat(project): basic dependencies and dummy code

--- src/main.rs
+++ src/main.rs
@@ -28,24 +28,60 @@ impl Query {
 // Request queries can be executed against a RootNode.
 type Schema = juniper::RootNode<'static, Query, EmptyMutation<Ctx>, EmptySubscription<Ctx>>;
 
-fn main() {
-    // Create a context object.
-    let ctx = Ctx(Episode::NewHope);
 
-    // Run the executor.
-    let (res, _errors) = juniper::execute_sync(
-        "query { favoriteEpisode }",
-        None,
-        &Schema::new(Query, EmptyMutation::new(), EmptySubscription::new()),
-        &Variables::new(),
-        &ctx,
-    ).unwrap();
 
-    // Ensure the value matches.
-    assert_eq!(
-        res,
-        graphql_value!({
-            "favoriteEpisode": "NEW_HOPE",
-        })
-    );
+
+
+
+
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+
+
+async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("Hello, World".into()))
 }
+
+#[tokio::main]
+async fn main() {
+    // We'll bind to 127.0.0.1:3000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // A `Service` is needed for every connection, so this
+    // creates one from our `hello_world` function.
+    let make_svc = make_service_fn(|_conn| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, Infallible>(service_fn(hello_world))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
+
+// fn main() {
+//     // Create a context object.
+//     let ctx = Ctx(Episode::NewHope);
+
+//     // Run the executor.
+//     let (res, _errors) = juniper::execute_sync(
+//         "query { favoriteEpisode }",
+//         None,
+//         &Schema::new(Query, EmptyMutation::new(), EmptySubscription::new()),
+//         &Variables::new(),
+//         &ctx,
+//     ).unwrap();
+
+//     // Ensure the value matches.
+//     assert_eq!(
+//         res,
+//         graphql_value!({
+//             "favoriteEpisode": "NEW_HOPE",
+//         })
+//     );
+// }
